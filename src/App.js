aaa00d50d10e0dd75b776b0251ff7a3b2d856ece
Tansd123 <letan850@gmail.com>
2022-06-03 21:48:34 +0700

    Show Hide Text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useInsertionEffect, useState } from "react";
 import "./style.css";
 
 export default function App() {
   const [Text, SetText] = useState("");
-  
+  const [show, setshow] = useState(false);
   return (
     <>
-
-    <p> {Text}</p>
+    <input onChange={e => SetText(e.target.value)}/>
+    
+    <button type= "button" onClick={() => setshow(!show)}>
+     {show === true ? "Hide" : "Show" }
+    </button>
+    {show && <p> {Text}</p>}
       
     </>
   );
